a51d965b4e04f29d75300c6cac7ec05d368303b3
Gustavo Vicente <gustavovicentejob@gmail.com>
2020-09-08 20:26:47 -0300

    Inputmask

--- AplicacaoDeEstudoASPNETMVC/App_Start/BundleConfig.cs
+++ AplicacaoDeEstudoASPNETMVC/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace AplicacaoDeEstudoASPNETMVC
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/inputmask").Include(
+                      "~/Scripts/jquery.maskedinput.js"));
         }
     }
 }
